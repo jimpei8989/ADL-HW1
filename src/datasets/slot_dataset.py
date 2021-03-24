8a6c18401a4bf9ab153ca526df8a8c7a4757a36c
Wu-Jun Pei <jimpei8989@gmail.com>
2021-03-24 23:44:33 +0800

    Add slot dataset

---
+++ src/datasets/slot_dataset.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+import torch
+from torch.utils.data import Dataset
+
+from utils.io import json_load
+
+
+class SlotDataset(Dataset):
+    TAGS = (
+        "O",
+        "B-date",
+        "I-date",
+        "B-time",
+        "I-time",
+        "B-people",
+        "I-people",
+        "B-first_name",
+        "B-last_name",
+    )
+
+    @classmethod
+    def load(cls, dataset_dir: Path, split: str, **kwargs):
+        data = json_load(dataset_dir / f"{split}.json")
+        return cls(data, **kwargs)
+
+    def __init__(self, data, tokenizer=None):
+        self.data = data
+        self.tokenizer = tokenizer
+        self.tag_to_idx = {label: i for i, label in enumerate(self.TAGS)}
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index: int):
+        sample = self.data[index]
+        ret = {
+            "id": sample["id"],
+            "input_ids": torch.as_tensor(
+                self.tokenizer.convert_tokens_to_ids(sample["tokens"]), dtype=torch.long
+            ),
+        }
+
+        if "tags" in sample:
+            ret.update(
+                {
+                    "tags": torch.as_tensor(
+                        [self.tag_to_idx[tag] for tag in sample["tags"]], dtype=torch.long
+                    )
+                }
+            )
+
+        return ret
--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -1,13 +1,16 @@
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data.dataloader import default_collate
 
-PAD_KEYS = set(["input_ids"])
+PADDING = {
+    "input_ids": 0,
+    "tags": -100,
+}
 
 
 def create_batch(samples):
     ret = {
-        k: pad_sequence([s[k] for s in samples], batch_first=False)
-        if k in PAD_KEYS
+        k: pad_sequence([s[k] for s in samples], batch_first=False, padding_value=PADDING[k])
+        if k in PADDING
         else default_collate([s[k] for s in samples])
         for k in samples[0]
     }
