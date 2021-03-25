1e9cb77fc6b4700f262efffcd3cd33a0ef411213
Wu-Jun Pei <jimpei8989@gmail.com>
2021-03-25 22:40:33 +0800

    Add padding, oov tokens

--- src/models/tokenizer.py
+++ src/models/tokenizer.py
@@ -6,23 +6,28 @@ from utils.io import json_load
 
 
 class Tokenizer:
-    UNKNOWN_TOKEN = "[UNKNOWN]"
+    PAD_TOKEN = "[PAD]"
+    OOV_TOKEN = "[OOV]"
 
     @classmethod
     def from_pretrained(cls, model_dir: Path):
         word_list = json_load(model_dir / "dictionary.json")
         embeddings = np.load(model_dir / "word_vectors.npy")
+
+        word_list = [cls.PAD_TOKEN, cls.OOV_TOKEN] + word_list
+        embeddings = np.concatenate(
+            [
+                np.zeros((1, embeddings.shape[1])),
+                embeddings.mean(axis=0).reshape(1, -1),
+                embeddings,
+            ]
+        )
+
         return cls(word_list, embeddings)
 
     def __init__(
         self, word_list: Optional[List[str]] = None, embeddings: Optional[np.ndarray] = None
     ):
-        if word_list and self.UNKNOWN_TOKEN not in word_list:
-            word_list = [self.UNKNOWN_TOKEN] + word_list
-
-            if embeddings is not None:
-                embeddings = np.concatenate([embeddings.mean(axis=0).reshape(1, -1), embeddings])
-
         self.word_list = word_list
         self.embeddings = embeddings
         self.token_to_idx = {token: i for i, token in enumerate(word_list)}
@@ -31,7 +36,7 @@ class Tokenizer:
         return self.convert_tokens_to_ids(self.tokenize(text))
 
     def convert_token_to_id(self, token):
-        return self.token_to_idx.get(token, 0)
+        return self.token_to_idx.get(token, 1)
 
     def convert_tokens_to_ids(self, tokens: List[str]) -> List[int]:
         return [self.convert_token_to_id(token) for token in tokens]
